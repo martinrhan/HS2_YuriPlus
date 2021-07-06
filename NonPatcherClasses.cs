01330aff13b6f4a07a719dd4b4d1efe5fc9be865
martinrhan <martinrhan@126.com>
2021-07-06 09:20:12 +0800

    Initial commit

---
+++ NonPatcherClasses.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Harmony;
+using BepInEx.Logging;
+using HarmonyLib;
+using HS2;
+using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace HS2_YuriPlus {
+    [BepInPlugin("HS2_YuriPlus", "Yuri Plus Plugin", "0.8")]
+    class Main : BaseUnityPlugin {
+        static internal Main Instance;
+        void Start() {
+            GameObject MainGameObject = new GameObject("HS2_YuriPlus");
+            MainGameObject.AddComponent<Monobehaviour_HS2_YuriPlus>();
+            Instance = this;
+            DontDestroyOnLoad(MainGameObject);
+            HarmonyLib.Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "HS2_YuriPlus");
+            Logger.LogMessage("HS2_YuriPlus Pathching Complete");
+            InitConfig();
+
+        }
+
+        void InitConfig() {
+            ConfigDef_NoDick = new ConfigDefinition("General", "No Dick");
+            this.Config.Bind<bool>(ConfigDef_NoDick, true,
+                new ConfigDescription("When enabled, all H motions with dick will be removed from the list."));
+            ConfigDef_NoMotionLimit = new ConfigDefinition("General", "No Motion Limit");
+            this.Config.Bind<bool>(ConfigDef_NoMotionLimit, true,
+                new ConfigDescription("When enabled, H motions will be loaded into the list without limit (except the no dick setting)."));
+        }
+
+        ConfigDefinition ConfigDef_NoDick;
+        public bool ConfigValue_NoDick { get { return (bool)(this.Config[this.ConfigDef_NoDick].BoxedValue); } }
+        ConfigDefinition ConfigDef_NoMotionLimit;
+        public bool ConfigValue_NoMotionLimit { get { return (bool)(this.Config[this.ConfigDef_NoMotionLimit].BoxedValue); } }
+
+        public static BepInEx.Logging.ManualLogSource GetLogger { get { return Instance.Logger; } }
+    }
+
+    public class Monobehaviour_HS2_YuriPlus : MonoBehaviour {
+        public static List<HScene.AnimationListInfo>[] lstAnimInfo;
+
+        internal static void Set() {
+            lstAnimInfo = HSceneManager.HResourceTables.lstAnimInfo;
+        }
+    }
+
+    internal static class CommonMethods {
+        internal static bool CheckHasDick(in HScene.AnimationListInfo lstAnimInfo) {
+            string[] splitedStrings = lstAnimInfo.assetpathFemale.Split('/');
+            string lastString = splitedStrings[splitedStrings.Length - 1];
+            return lastString == "houshi.unity3d" || lastString == "sonyu.unity3d" || lastString == "3p.unity3d";
+        }
+    }
+}
---
+++ PatcherClasses.cs
@@ -0,0 +1,109 @@
+﻿using BepInEx.Configuration;
+using HarmonyLib;
+using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HS2_YuriPlus {
+    [HarmonyPatch]
+    static class Patcher_GameCharaFileInfoAssist {
+        [HarmonyPatch(typeof(GameLoadCharaFileSystem.GameCharaFileInfoAssist), "CreateCharaFileInfoList")]
+        [HarmonyPrefix]
+        static bool Prefix_CreateCharaFileInfoList(ref bool useFemaleFutanariOnly) {
+            ////Main.GetLogger.LogDebug("Prefix_CreateCharaFileInfoList Called");
+            useFemaleFutanariOnly = false;
+            return true;
+        }
+
+    }
+
+
+    [HarmonyPatch]
+    static class Patcher_HSceneSprite {
+        [HarmonyPatch(typeof(HSceneSprite), "CheckMotionLimit")]
+        [HarmonyPrefix]
+        static bool Prefix_CheckMotionLimit(ref bool __result, in HScene.AnimationListInfo lstAnimInfo) {
+            //Main.GetLogger.LogDebug("111");
+            if (Main.Instance.ConfigValue_NoDick && CommonMethods.CheckHasDick(lstAnimInfo)) {
+                //Main.GetLogger.LogDebug("222");
+                __result = false;
+                return false;
+            }
+            if (Main.Instance.ConfigValue_NoMotionLimit) {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(HSceneSprite), "CheckMotionLimitAppend3P")]
+        [HarmonyPrefix]
+        static bool Prefix_CheckMotionLimitAppend3P(ref bool __result, in HScene.AnimationListInfo lstAnimInfo) {
+            //Main.GetLogger.LogDebug("a");
+            if (Main.Instance.ConfigValue_NoDick && CommonMethods.CheckHasDick(lstAnimInfo)) {
+                //Main.GetLogger.LogDebug("b");
+                __result = false;
+                return false;
+            }
+            if (Main.Instance.ConfigValue_NoMotionLimit) {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(HSceneSprite), "CheckPlace", new Type[] { typeof(HScene.AnimationListInfo) })]
+        [HarmonyPrefix]
+        static bool Prefix_CheckPlace(ref bool __result, in HScene.AnimationListInfo info) {
+            //Main.GetLogger.LogDebug("aa");
+            if (Main.Instance.ConfigValue_NoDick && CommonMethods.CheckHasDick(info)) {
+                //Main.GetLogger.LogDebug("bb");
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    [HarmonyPatch]
+    static class Pather_HSceneManager_HSceneTables {
+        [HarmonyPatch(typeof(HSceneManager.HSceneTables), "LoadAnimationFileName")]
+        [HarmonyPostfix]
+        static void Postfix_LoadAnimationFileName() {
+            Monobehaviour_HS2_YuriPlus.Set();
+        }
+
+    }
+
+    [HarmonyPatch]
+    static class Patcher_HScene {
+        private static Manager.HSceneManager hSceneManager;
+        [HarmonyPatch(typeof(HScene),"Start")]
+        [HarmonyPrefix]
+        static bool Prefix_Start() {
+            hSceneManager = Singleton<HSceneManager>.Instance;
+            Main.GetLogger.LogDebug(hSceneManager.player);
+            if (!hSceneManager.bFutanari) {
+                hSceneManager.bFutanari = true;
+                //IsModified = true;
+            }
+            return true;
+        }
+        /*
+        private static bool IsModified = false;
+
+        [HarmonyPatch(typeof(HScene),"Start")]
+        [HarmonyPostfix]
+        static void Postfix_Start() {
+            if (IsModified) {
+                hSceneManager.bFutanari = true;
+                IsModified = false;
+            }
+        }*/
+    }
+
+}
